1abd1094d7f6823b224d6ef5e2437992e844b05b
churianne <aleinir16@gmail.com>
2026-09-10 02:23:09 +0800

    Update MainActivity.kt

--- src/MainActivity.kt
+++ src/MainActivity.kt
@@ -1,1 +1,10 @@
+package com.example.camera // Replace with the package name defined in your AndroidManifest.xml
 
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
